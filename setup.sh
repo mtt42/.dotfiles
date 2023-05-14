6dc1c963f96ab0dfd4a7b456a1c09602a69285a8
mtt42 <19205795+mtt42@users.noreply.github.com>
2023-05-14 17:32:36 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set up newly created machine.
+
+username=$1
+
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
+# Check if user already exists
+if id "$username" >/dev/null 2>&1; then
+   echo "User $username already exists. SKIPPED"
+else
+   # Create the user
+   adduser --disabled-password --gecos "" "$username"
+
+   # Add user to the sudo group
+   usermod -aG sudo "$username"
+
+   # Display success message
+   echo "User $username has been created and added to the sudo group."
+fi
+
+# Transfer SSH keys if the user exists
+if id "$username" >/dev/null 2>&1; then
+   rsync --archive --chown=$username:$username ~/.ssh /home/$username
+   echo "SSH keys transferred to user $username."
+else
+   echo "Unable to transfer SSH keys. User $username does not exist."
+fi
